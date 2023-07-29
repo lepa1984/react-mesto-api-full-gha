9add155e2df422e37d53c0a53e0394061379fd04
Alexey Popov <lepa1984@mail.ru>
2023-07-29 15:22:02 +0300

    sprint 15, 1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -48,13 +48,9 @@ const login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'super-secret',
-        {
-          expiresIn: '7d',
-        }
-      );
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'unique-secret-key', {
+        expiresIn: '7d',
+      });
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,18 +2,18 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
-  const authorization = req.cookies.jwt;
-  if (!authorization) {
+  const extractToken = (header) => header.replace('jwt=', '');
+  if (!req.headers.cookie || !req.headers.cookie.startsWith('jwt=')) {
     next(new AuthError('Необходима авторизация'));
     return;
   }
-  const token = authorization.replace('Bearer ', '');
+
   let payload;
 
   try {
     payload = jwt.verify(
-      token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'super-secret'
+      extractToken(req.headers.cookie),
+      NODE_ENV === 'production' ? JWT_SECRET : 'unique-secret-key'
     );
   } catch (err) {
     next(new AuthError('Необходима авторизация'));
